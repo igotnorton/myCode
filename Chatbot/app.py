636bd2d3059903142fca764a9ea495818e32d9ea
igotnorton <kamiloleskiewicz@gmail.com>
2022-10-20 09:04:20 +0100

    modified:   Chatbot/__pycache__/app.cpython-310.pyc 	modified:   Chatbot/__pycache__/chatbot.cpython-310.pyc 	modified:   Chatbot/app.py 	modified:   Chatbot/static/style.css 	modified:   Chatbot/templates/base.html 	modified:   Chatbot/templates/chatbot.html 	modified:   Chatbot/templates/index.html

--- Chatbot/app.py
+++ Chatbot/app.py
@@ -5,8 +5,8 @@ from time import sleep
 
 app = Flask(__name__)
 app.config['SECRET__KEY'] = 'a_very_secretive_key_123456789'
-answer_list = []                            
-delay = sleep(0.03)
+answer_list = []
+delay = sleep(1)
 
 @app.route("/")
 def home():
@@ -14,21 +14,25 @@ def home():
     answer_list.clear()
     return render_template("index.html")
 
-
 @app.route("/chatbot", methods=["GET", "POST"])
 def chatbot():
-    global answer_list, delay
-    if request.method == "POST":
-        message = request.form['message']
-        ints = predict_class(message)
-        response = get_response(ints, intents)
-        answer_list.append(response)
-        print(answer_list)
-        if len(answer_list) > 4:
-            answer_list.remove(answer_list[0])
-        return render_template("chatbot.html", message=message, answer_list=answer_list, delay=delay)
-    return render_template("chatbot.html", message="", answer_list=answer_list)
-
+  global answer_list, delay
+  if len(answer_list)>=5:
+    answer_list.pop(0)
+  if request.method == "POST":
+    message = request.form['message']
+    ints = predict_class(message)
+    response = get_response(ints, intents)
+    answer_list.append(response)
+    print(answer_list)
+  return render_template("chatbot.html", message="", answer_list=answer_list, delay=delay)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+  app.debug = True
+  app.run()
+
+      # del answer_list[-1]
+      # answer_list = answer_list[1:]
+      # answer_list.clear()
+      # answer_list.remove(answer_list[0])
+      # answer_list.pop(0)
